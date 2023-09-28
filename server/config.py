ff6b0a9efc69e282f6b6e85a33acd185f12cff5e
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-28 15:18:34 -0500

    create seed.py file

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import MetaData
 
 # Local imports
@@ -17,15 +18,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 # Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-db = SQLAlchemy(metadata=metadata)
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+# db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
 
 # Instantiate REST API
 api = Api(app)
 
+bcrypt = Bcrypt(app)
+
 # Instantiate CORS
 CORS(app)
+
+from models import User, Question, Answer, QuizAttempt
--- server/models.py
+++ server/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 from datetime import datetime
 
-from config import db
+from config import db, bcrypt
 
 # Define the association table for the many-to-many relationship
 user_question =  db.Table(
@@ -12,7 +13,7 @@ user_question =  db.Table(
 )
 
 
-# USer Model
+# User Model
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
@@ -23,6 +24,16 @@ class User(db.Model, SerializerMixin):
 
     # many-to-many relationship with Question
     questions_attempted = db.relationship("Question", secondary=user_question, back_populates="users_attempted")
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+        raise Exception("Cannot access password hash")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
 
     def __repr__(self):
         return f"User \
