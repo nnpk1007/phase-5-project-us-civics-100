e1aa9203a726f6199abe76ac0edf1f95c5a982dc
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-28 06:57:42 -0500

    define the asscociation table user_question

--- server/models.py
+++ server/models.py
@@ -3,4 +3,10 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+# Define the association table for the many-to-many relationship
+user_question =  db.Table(
+    "user_questions",
+    db.Column("user_id", db.Integer, db.ForeignKey("user_id", primary_key=True)),
+    db.Column("question_id", db.Integer, db.ForeignKey("question_id", primary_key=True))
+)
+
