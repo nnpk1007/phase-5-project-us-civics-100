8ef9ab7658528cc9fd39bde1cd15b6bfc46e31bc
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-28 07:13:10 -0500

    Add User model

--- server/models.py
+++ server/models.py
@@ -6,7 +6,28 @@ from config import db
 # Define the association table for the many-to-many relationship
 user_question =  db.Table(
     "user_questions",
-    db.Column("user_id", db.Integer, db.ForeignKey("user_id", primary_key=True)),
-    db.Column("question_id", db.Integer, db.ForeignKey("question_id", primary_key=True))
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id", primary_key=True)),
+    db.Column("question_id", db.Integer, db.ForeignKey("question.id", primary_key=True))
 )
 
+
+# USer Model
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    # many-to-many relationship with Question
+    questions_attempted = db.relationship("Question", secondary=user_question, back_populates="users_attempted")
+
+    def __repr__(self):
+        return f"User \
+            id : {self.id} \
+            username: {self.username} \
+            email: {self.email} \
+            "
+
+    
