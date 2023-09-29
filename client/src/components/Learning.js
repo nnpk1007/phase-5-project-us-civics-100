4ee5dc033c31064aa6c705e37ad2457f72fb6650
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-29 13:05:27 -0500

    implement code in Learning.js to show question number

--- client/src/components/Learning.js
+++ client/src/components/Learning.js
@@ -13,13 +13,15 @@ function Learning() {
     return (
         <div className="container mt-5">
             <h1 className="text-center mt-5">Learning Mode</h1>
+            <p>* If you are 65 years old or older and have been a legal permanent resident of the United States for 20 or more years, you 
+                may study just the questions that have been marked with an asterisk</p>
 
-            {questions.map(question => (
+            {questions.map((question, index) => (
                 <div key={question.id}>
-                    <p><strong>{question.id} {question.question_text}</strong></p>
+                    <p><strong>Question {index+1}: {question.question_text}</strong></p>
 
                     {question.answers.map(answer => (
-                        <p key={answer}>{answer}</p>
+                        <p key={answer}>. {answer}</p>
                     ))}
                 </div>
             ))}
