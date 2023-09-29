ed1e99feaf2fe6398254198448822087d66169e3
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-29 15:38:55 -0500

    implement code in Learning.js to show a navbar: QuizTest and Login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,15 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home"
 import Learning from "./Learning"
+import Login from "./Login"
+import Signup from "./Signup"
+import QuizTest from "./QuizTest"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/learning" element={<Learning />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/test" element={<QuizTest />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/Learning.js
+++ client/src/components/Learning.js
@@ -1,33 +1,51 @@
-import { useState, useEffect } from "react"
-
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function Learning() {
-    const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([]);
 
-    useEffect(() => {
-        fetch("/civics100-learning")
-            .then(res => res.json())
-            .then(data => setQuestions(data))
-    }, [])
+  useEffect(() => {
+    fetch("/civics100-learning")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }, []);
 
-    return (
-        <div className="container mt-5">
-            <h1 className="text-center mt-5">Learning Mode</h1>
-            <p>* If you are 65 years old or older and have been a legal permanent resident of the United States for 20 or more years, you 
-                may study just the questions that have been marked with an asterisk</p>
-
-            {questions.map((question, index) => (
-                <div key={question.id}>
-                    <p><strong>Question {index+1}: {question.question_text}</strong></p>
-
-                    {question.answers.map(answer => (
-                        <p key={answer}>. {answer}</p>
-                    ))}
-                </div>
-            ))}
+  return (
+    <>
+      <nav className="navbar navbar-expand fixed-top navbar-light bg-light">
+        <div className="navbar-nav ms-auto">
+          <NavLink className="nav-link" style={{ color: "purple" }} to="/test">
+            QuizTest
+          </NavLink>
+          <NavLink className="nav-link" style={{ color: "purple" }} to="/login">
+            Login
+          </NavLink>
         </div>
-    )
+      </nav>
+      <div className="container mt-5">
+        <h2 className="text-center mt-5">Learning Mode</h2>
+        <p>
+          * If you are 65 years old or older and have been a legal permanent
+          resident of the United States for 20 or more years, you may study just
+          the questions that have been marked with an asterisk
+        </p>
 
+        {questions.map((question, index) => (
+          <div key={question.id}>
+            <p>
+              <strong>
+                Question {index + 1}: {question.question_text}
+              </strong>
+            </p>
+
+            {question.answers.map((answer) => (
+              <p key={answer}>. {answer}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Learning
+export default Learning;
