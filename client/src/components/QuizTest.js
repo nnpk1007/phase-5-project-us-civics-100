bc78dcf13a4c2ddcbfb5b889fbe1564f78ac5abc
Ngoc Nguyen <nnpk0410@gmail.com>
2023-10-04 11:31:36 -0500

    implement code to show score in QuizTest component and fix that only 9 questions showed to 10 questions showed

--- client/src/components/QuizTest.js
+++ client/src/components/QuizTest.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 function QuizTest() {
   const [quiz, setQuiz] = useState([]);
   const [currentQuestionsIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     fetch("/civics-test")
@@ -13,55 +15,86 @@ function QuizTest() {
       });
   }, []);
 
-  console.log(quiz);
-  
+  console.log("Length:", quiz.length);
+  console.log("Current question index:", currentQuestionsIndex)
+
+  const handleAnswerSelect = (answerIndex) => {
+    setSelectedAnswerIndex(answerIndex);
+  };
+
   const handleNextQuestion = () => {
-    // check to make sure the currenQuestionIndex does not go over the limit
-    if (currentQuestionsIndex < quiz.length -1) {
-      setCurrentQuestionIndex(currentQuestionsIndex + 1)
+    // Check if the selected answer is correct and update the score.
+    const currentQuestion = quiz[currentQuestionsIndex];
+    if (
+      selectedAnswerIndex !== null &&
+      currentQuestion.answer_options[selectedAnswerIndex].correct
+    ) {
+      setScore(score + 1);
     }
-  }
+    // check to make sure the currenQuestionIndex does not go over the limit
+    if (currentQuestionsIndex < quiz.length) {
+      setCurrentQuestionIndex(currentQuestionsIndex + 1);
+    }
+  };
+  console.log("Score:", score);
 
   return (
     <>
       <div className="container-fluid">
         <div className="jumborton">
-          <h2 className="text-center mt-5">US Civics Test</h2>
-        {currentQuestionsIndex < quiz.length &&  ( 
-          <div className="card border-info">
-            <div className="card-header bg-info text-white">
-              Question {currentQuestionsIndex + 1}:{" "}
-              {quiz[currentQuestionsIndex].question_text}
-            </div>
-            {quiz[currentQuestionsIndex].answer_options.map(
-              (answer, answerIndex) => (
-                <div className="form-check" key={answerIndex}>
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name={`q${currentQuestionsIndex}`}
-                    id={`q${currentQuestionsIndex}_a${answerIndex}`}
+          <h2 className="text-center text-primary mt-5">US Civics Test</h2>
+          {currentQuestionsIndex < quiz.length ? (
+            <div className="card border-info">
+              <div className="card-header bg-info text-white">
+                Question {currentQuestionsIndex + 1}:{" "}
+                {quiz[currentQuestionsIndex].question_text}
+              </div>
+              {quiz[currentQuestionsIndex].answer_options.map(
+                (answer, answerIndex) => (
+                  <div className="form-check" key={answerIndex}>
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name={`q${currentQuestionsIndex}`}
+                      id={`q${currentQuestionsIndex}_a${answerIndex}`}
+                      onChange={() => handleAnswerSelect(answerIndex)}
                     />
-                  <label
-                    className="form-check-label"
-                    htmlFor={`q${currentQuestionsIndex}_a${answerIndex}`}
+                    <label
+                      className="form-check-label"
+                      htmlFor={`q${currentQuestionsIndex}_a${answerIndex}`}
                     >
-                    {answer.answer_text}
-                  </label>
+                      {answer.answer_text}
+                    </label>
+                  </div>
+                )
+              )}
+              <button
+                type="button"
+                className="btn btn-success mt-3"
+                onClick={handleNextQuestion}
+              >
+                Next
+              </button>
+            </div>
+          ) : (
+            <div className="text-center">
+              <h3 className="text-info">Quiz Completed</h3>
+              <p className="score fs-4 fw-bold text-secondary">Score: {score}</p>
+              {score > 5 ? (
+                <div className="text-center">
+                  <h2 className="text-success">
+                    Congratulations! You passed the Civics test
+                  </h2>
                 </div>
-              )
-            )}
-          <button
-            type="button"
-            className="btn btn-success mt-3"
-            onClick={handleNextQuestion}
-            >
-              Next
-          </button>
-          </div>
-        )}
+              ) : (
+                <div className="text-center">
+                  <h2 className="text-danger">Try again next time</h2>
+                </div>
+              )}
+            </div>
+          )}
         </div>
-        </div>
+      </div>
     </>
   );
 }
