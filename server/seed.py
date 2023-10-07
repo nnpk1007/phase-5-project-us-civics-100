0bf204b488a4367c942b982eadef53c33f8cbedb
Ngoc Nguyen <nnpk0410@gmail.com>
2023-10-06 23:26:36 -0500

    add README file

--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-# from random import randint, choice as rc
-
-# # Remote library imports
-# from faker import Faker
-
-# Local imports
 from app import app
 from models import db, User, Question, Answer, QuizAttempt
 
